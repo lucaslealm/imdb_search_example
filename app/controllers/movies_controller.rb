115e5dfa457c19508be035c0b9197fedbcee4385
Lucas Martins <lucaslealm@gmail.com>
2020-11-03 15:30:42 -0300

    implemented director search

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,16 @@
 class MoviesController < ApplicationController
     def index
-      @movies = Movie.all
+      if params[:search][:query].present?
+        sql_query = " \
+          movies.title @@ :query \
+          OR movies.syllabus @@ :query \
+          OR directors.first_name @@ :query \
+          OR directors.last_name @@ :query \
+        "
+        @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:search][:query]}%")
+      else
+        @movies = Movie.all
+      end
     end
   end
   
